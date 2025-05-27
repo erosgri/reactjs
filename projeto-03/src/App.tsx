e526a841be623e17d50e51537ebfb2c97957f81d
erosgri <junior-gri@live.com>
2025-05-27 07:12:40 -0300

    template concluido e ajustado .

--- projeto-03/src/App.tsx
+++ projeto-03/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container-app'>
+      <div className='container-app__header'>
+        <div className='container-app__mask' />
+        <h1>Tarefas</h1>
+
+        <input type="text" placeholder="Criar uma nova tarefa" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <ul className='content-tasks'>
+        <li className='task-item'>Tarefa 1</li>
+        <li className='task-item'>Tarefa 1</li>
+        <li className='task-item'>Tarefa 1</li>
+        <li className='task-item'>Tarefa 1</li>
+        <li className='task-item'>Tarefa 1</li>
+      </ul>
+
+    </div>
   )
 }
 
 export default App
+
+// alt + shift + o remove imports desnecessarios!!!
